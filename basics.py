6cebed6f9cc2a0decaabf5a8aea4961bd77adea3
Shantanu Kundu <kundushantanu36@gmail.com>
2023-07-24 10:43:13 +0530

    Done

---
+++ basics.py
@@ -0,0 +1,82 @@
+print('hi',34)
+n=input('Enter a number:')
+print(n)
+
+p=input('Enter a expression:')
+print(p)
+print(eval(p))
+
+s='hello this is new process'
+print(s)
+len(s)
+type(s)
+
+s[0]
+s[-1]
+# slicing [begin:end(exclusive):step size]
+s[-7:28:1]
+s[-1:-8:-1]
+
+s[:10]
+s[-11:]
+s[:-8:-1]
+s[4::-1]
+s[::-1]
+
+'hello' * 6
+'hi'+'bye'
+s+= ' here'
+s
+s=s[:5] + 'ankit' + s[5:]
+s
+a=[21,222,23,34]
+print(a)
+print(len(a))
+print(type(a))
+
+a[0]
+a[-2]
+type(a[-2])
+a[-2] [-1]
+[4,7,5] * 3
+[3,5,6] + ['hi',9]
+a=a+[3462]
+a
+
+a=[4,5,6,7,'hi']
+a
+a+={'all':4,'bye':5}
+a
+
+a+= {3,67,0}
+a
+
+d={'a1':4,'b1':3}
+print (d)
+print(len(d))
+print(type(d))
+print(d['a1'])
+
+d['c1']=[7,4,5,6]
+d
+
+d['b1']='hello everyone'
+d
+e={3,4,5,6,3,2,34}
+e=e+{2,3,4}
+e[0]
+3>5
+bool(9)
+bool(0)
+
+print(bool(''))
+print(bool([]))
+print(bool(()))
+print(bool({}))
+print(bool(None))
+
+
+
+
+
+
